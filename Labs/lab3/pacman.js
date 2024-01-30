c1879b02113612bece7317b3f9df9b432cfae688
Alexandre Ringuette <aring057@uottawa.ca>
2024-01-30 13:43:27 -0500

    complete level

--- Labs/lab3/pacman.js
+++ Labs/lab3/pacman.js
@@ -1,6 +1,9 @@
 let pacmanPos = 0;
 let ghostPos = 0;
 let fruitPos = 0;
+let score = 0;
+let level = 1;
+
 function createGame(n){
     let table = [];
     for(i = 0; i<=n; i++){
@@ -18,17 +21,38 @@ function createGame(n){
 }
 
 function moveLeft(game){
-    game[pacmanPos] = '.';
-    game[pacmanPos-1] = 'C';
-    pacmanPos--;
+    game[pacmanPos] = '';
+    if(pacmanPos-1 < 0){
+        game[game.length-1] = 'C';
+        pacmanPos = game.length-1;
+    } else{
+        game[pacmanPos-1] = 'C';
+        pacmanPos--;
+
+    }
+    score++;
     return game;
 }
 
 function moveRight(game){
-    game[pacmanPos] = '.';
-    game[pacmanPos+1] = 'C';
-    pacmanPos++;
+    game[pacmanPos] = '';
+    if(pacmanPos+1 > game.length-1){
+        game[0] = 'C';
+        pacmanPos = 0;
+    } else{
+        game[pacmanPos+1] = 'C';
+        pacmanPos++;
+    }
+    score++
     return game;
+}
+
+function checkBoardComplete(game){
+    if(game.includes('.')) return false;
+    else{
+        level++;
+        return createGame(game.length);
+    } 
 }
 
 let game = createGame(10);
@@ -36,3 +60,15 @@ let game = createGame(10);
 console.log(game);
 console.log(moveLeft(game));
 console.log(moveRight(game));
+console.log(moveRight(game));
+console.log(moveRight(game));
+console.log(moveRight(game));
+console.log(moveRight(game));
+console.log(moveRight(game));
+console.log(moveRight(game));
+console.log(moveRight(game));
+console.log(moveRight(game));
+console.log(checkBoardComplete(game));
+console.log(moveRight(game));
+
+
