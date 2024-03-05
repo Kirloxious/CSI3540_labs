85bdd99bd3ff0d74a107560ce66b464094ec2e93
Alexandre Ringuette <aring057@uottawa.ca>
2024-03-05 12:33:47 -0500

    start of php game

---
+++ Tutorials/Tutorial6/tut3.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+
+<body>
+    <?php print("Hello World"); ?>
+    <?php
+    $name = "Paul";
+    print("$Name");
+    ?><!-- end PHP script -->
+</body>
+
+Describe how input from an HTML5 form is retrieved in a PHP program.
+The form info is sent to a php file which receives the info as a POST then the php program executes
+
+</html>
